96f2eae4d7016b0dc726ce8b34c93d2869e4aafc
joseph <josephlin117@gmail.com>
2021-04-17 15:49:19 +0800

    fix bug

--- app.py
+++ app.py
@@ -21,9 +21,10 @@ def show():
     item_name = request.args.get('item_name')
     #print(input_name)
     #items = Price.query.filter_by(name = '实木课桌凳').all()
+    #print(items)
     price_items = Price.query.filter_by(name = item_name).all()
     #print(price_items)
     return render_template('show.html', items=price_items)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    app.run('0.0.0.0', port='5000', debug=True)
